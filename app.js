15616d2a592cde252116ca47dadb490fb99020a9
Dineshkumar <sendmail2dk@gmail.com>
2024-11-07 12:08:01 +0530

    mongo atlas url updated

--- app.js
+++ app.js
@@ -1,79 +1,85 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-const url = require('url')
-const session = require('express-session')
-const mongoose = require('mongoose')
+const express = require("express");
+const app = express();
+const path = require("path");
+const url = require("url");
+const session = require("express-session");
+const mongoose = require("mongoose");
 
 //routers
-const indexRoute = require('./routes/index')
-const userRoute = require('./routes/mongoDB/user')
-const subscribeRoute = require('./routes/subscribe')
-const searchFlightRoute = require('./routes/searchFlight')
-const dataRoute = require('./routes/data');
-
+const indexRoute = require("./routes/index");
+const userRoute = require("./routes/mongoDB/user");
+const subscribeRoute = require("./routes/subscribe");
+const searchFlightRoute = require("./routes/searchFlight");
+const dataRoute = require("./routes/data");
 
 //template engine
-app.set('view engine', 'pug')
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
 
 //static files
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "public")));
 
 //json
-app.use(express.json())
+app.use(express.json());
 
 //url encoded for post requests
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/air_ticket')
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.log('Failed to connect to MongoDB', err));
+mongoose
+  .connect(
+    "mongodb+srv://sendmail2dk:Y9RSjpFUntsyxH7E@cluster0.7br9d.mongodb.net/air-ticket-db?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("Failed to connect to MongoDB", err));
 
 // Session management
-app.use(session({
-  secret: 'your_secret_key', // Change this to a secure secret
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false } // Set to true if using HTTPS
-}));
+app.use(
+  session({
+    secret: "your_secret_key", // Change this to a secure secret
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }, // Set to true if using HTTPS
+  })
+);
 
 //routing
-app.get('/', indexRoute)
-app.use('/user', userRoute)
-app.post('/subscribe', subscribeRoute)
-app.post('/search_flight', searchFlightRoute)
-app.use('/data', dataRoute)
+app.get("/", indexRoute);
+app.use("/user", userRoute);
+app.post("/subscribe", subscribeRoute);
+app.post("/search_flight", searchFlightRoute);
+app.use("/data", dataRoute);
 
 // Middleware - Static pages
 app.use((req, res, next) => {
   try {
-      // Render the requested page by extracting the pathname from the URL
-      res.render(url.parse(req.url, true).pathname.substring(1), {userId: req.session.userId});
+    // Render the requested page by extracting the pathname from the URL
+    res.render(url.parse(req.url, true).pathname.substring(1), {
+      userId: req.session.userId,
+    });
   } catch (error) {
-      // Create a new error and pass it to the next middleware
-      const err = new Error('Error rendering the page');
-      err.status = 500;
-      return next(err); // Forward to error-handling middleware
+    // Create a new error and pass it to the next middleware
+    const err = new Error("Error rendering the page");
+    err.status = 500;
+    return next(err); // Forward to error-handling middleware
   }
 });
 
 //middleware - error handling
 app.use((err, req, res, next) => {
-  console.log('error called')
+  console.log("error called");
   // Set a default status code (e.g., 500 Internal Server Error if no specific status)
   const statusCode = err.status || 500;
 
   // Pass the status and error message to the Pug template
-  res.status(statusCode).render('error', {
-      title: `Error ${statusCode}`,
-      statusCode: statusCode,
-      message: err.message || 'Something went wrong. Please try again later.'
+  res.status(statusCode).render("error", {
+    title: `Error ${statusCode}`,
+    statusCode: statusCode,
+    message: err.message || "Something went wrong. Please try again later.",
   });
 });
 
-const port = 3000
-app.listen(port, ()=>{
-  console.log(`Server is running @ http://localhost:${port}`)
-})
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running @ http://localhost:${port}`);
+});
